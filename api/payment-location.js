6f81ca3da4d1fb3e1577310240c54711f97bdcdc
s8mrbmg7cj-cyber <andrewyoung02@icloud.com>
2026-04-17 17:13:13 -0600

    Create payment-location.js

---
+++ api/payment-location.js
@@ -0,0 +1,14 @@
+export default function handler(req, res) {
+  const twiml = `<?xml version="1.0" encoding="UTF-8"?>
+<Response>
+  <Gather numDigits="1" action="https://missed-call-rescue-nu.vercel.app/api/payment-send" method="POST">
+    <Say voice="alice">
+      For Bonham, press 1. For Highway 136, press 2.
+    </Say>
+  </Gather>
+  <Say voice="alice">We did not receive a selection. Goodbye.</Say>
+</Response>`;
+
+  res.setHeader("Content-Type", "text/xml");
+  res.status(200).send(twiml);
+}
